d1544993de896d9bfb9d31b76d04f82276e717eb
NamSangwon <127469500+NamSangwon@users.noreply.github.com>
2025-06-20 17:38:36 +0900

    feat #1 : SiteUser 레포지토리 구성

---
+++ src/main/java/com/studyhub/backend_user/domain/repository/SiteUserRepository.java
@@ -0,0 +1,7 @@
+package com.studyhub.backend_user.domain.repository;
+
+import com.studyhub.backend_user.domain.SiteUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SiteUserRepository extends JpaRepository<SiteUser, Long> {
+}
