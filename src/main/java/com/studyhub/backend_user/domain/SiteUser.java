a86297c57b16f2a85a235af01ed7f970265badce
NamSangwon <127469500+NamSangwon@users.noreply.github.com>
2025-06-21 16:17:59 +0900

    fix #1 : JpaAuditing 사용 X

--- src/main/java/com/studyhub/backend_user/domain/SiteUser.java
+++ src/main/java/com/studyhub/backend_user/domain/SiteUser.java
@@ -4,8 +4,6 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
 
@@ -35,12 +33,10 @@ public class SiteUser {
     @Getter @Setter
     private String phoneNumber;
 
-    @CreatedDate
     @Column(name = "created_at", nullable = false, updatable = false)
     @Getter @Setter
     private LocalDateTime createdAt;
 
-    @LastModifiedDate
     @Column(name = "updated_at", nullable = false)
     @Getter @Setter
     private LocalDateTime updatedAt;
