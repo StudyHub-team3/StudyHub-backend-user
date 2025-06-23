37e8bcff23c88c6884468e7127010f69a9315431
NamSangwon <127469500+NamSangwon@users.noreply.github.com>
2025-06-23 16:33:50 +0900

    fix #3 : 토큰 저장 방식 변경 (mysql -> redis)

--- src/main/java/com/studyhub/backend_user/domain/repository/RefreshTokenRepository.java
+++
@@ -1,8 +0,0 @@
-package com.studyhub.backend_user.domain.repository;
-
-import com.studyhub.backend_user.domain.RefreshToken;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
-    RefreshToken findByToken(String token);
-}
